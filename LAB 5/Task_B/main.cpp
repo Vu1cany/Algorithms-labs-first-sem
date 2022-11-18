ce507531d5d7e2b3ad0bdf3578c000acc5cf4a12
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2022-11-18 23:50:48 +0300

    Task_B was added (doesn't work)

---
+++ LAB 5/Task_B/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+struct Thee {
+    int data = 0;
+    int leftChild = 0;
+    int rightChild = 0;
+};
+
+bool IsBinarySearchTree() {
+
+}
+
+int main() {
+    int n;
+    cin >> n;
+    Thee arr[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> arr->data >> arr->leftChild >> arr->rightChild;
+    }
+
+
+    return 0;
+}
