41a0070fc48a4fb71564ce8451518efd7b2af30d
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2022-11-25 13:22:41 +0300

    Doesn't work root delete

--- LAB 5/Task_D/main.cpp
+++ LAB 5/Task_D/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-struct BinarySearchThree{
+struct  BinarySearchThree{
 private:
 
     struct Node{
@@ -10,6 +10,8 @@ private:
         Node *parent = nullptr;
     };
 
+
+
 public:
 
     Node *root = nullptr;
@@ -17,6 +19,12 @@ public:
     void Insert(Node *X,int _data) {
         Node *p = new Node;
         p->data = _data;
+
+        if (X == nullptr) {
+            root = p;
+            return;
+        }
+
         while (X != nullptr) {   //
             if (p->data > X->data) {
                 if (X->right != nullptr) {
@@ -38,32 +46,148 @@ public:
         }
     }
 
-    Node Search (Node *X,int _data) {
-        if (X != nullptr || X->data == _data) {
-            return *X;
-        } else if (_data < X->data) {
+    Node *Search (Node *X, int _data) {
+        if (X == nullptr || X->data == _data) {
+            return X;
+        }
+        if (_data < X->data) {
             return Search(X->left, _data);
         } else {
             return Search(X->right, _data);
         }
     }
 
-    void Next() {
-
+    Node *Next(Node *X, int _data) {
+        Node *ans = nullptr;
+        while (X != nullptr) {
+            if (X->data > _data) {
+                ans = X;
+                X = X->left;
+            } else {
+                X = X->right;
+            }
+        }
+        return ans;
     }
 
-    void Prev() {
-
+    Node *Prev(Node *X, int _data) {
+        Node *ans = nullptr;
+        while (X != nullptr) {
+            if (X->data < _data) {
+                ans = X;
+                X = X->right;
+            } else {
+                X = X->left;
+            }
+        }
+        return ans;
     }
 
-    void Delete() {
+    void Delete(int _data) {
+        Node *p = Search(root, _data);
+        if (p == nullptr) return;
+        Node *parent = p->parent;
 
+        if (p == root) {
+            if (p->left == nullptr && p->right == nullptr) {
+                root = nullptr;
+
+            } else if (p->left == nullptr || p->right == nullptr) {
+                if (p->left == nullptr) {
+                    p->right->parent = nullptr;
+                    root = p->right;
+                } else {
+                    p->left->parent = nullptr;
+                    root = p->left;
+                }
+
+            } else {
+                Node *NewRoot = Next(root, root->data);
+                NewRoot->left = root->left;
+                root->left->parent = NewRoot;
+                root = root->right;
+                root->parent = nullptr;
+
+            }
+            return;
+        }
+
+        if (p->left == nullptr && p->right == nullptr) {
+            if (p == parent->right) {
+                parent->right = nullptr;
+            } else {
+                parent->left = nullptr;
+            }
+
+        } else if (p->left == nullptr || p->right == nullptr) {
+            if (p->left != nullptr) {
+                if (p == parent->right) {
+                    parent->right = p->left;
+                } else {
+                    parent->left = p->left;
+                }
+                p->left->parent = parent;
+
+            } else {
+                if (p == parent->right) {
+                    parent->right = p->left;
+                } else {
+                    parent->left = p->left;
+                }
+                p->right->parent = parent;
+            }
+
+        } else {
+            Node *next = Next(root, p->data);
+            p->data = next->data;
+            if (next->parent->left == next) {
+                next->parent->left = next->right;
+            } else {
+                next->parent->right = next->right;
+            }
+            if (next->right != nullptr) {
+                next->right->parent = next->parent;
+            }
+        }
     }
 
 };
 
 int main() {
     BinarySearchThree myThree;
-    std::cout << ;
+    while (!std::cin.eof()) {
+        std::string commands[5] = {"insert", "delete", "exists", "next", "prev"};
+        char input_command[10];
+        int val;
+        std::cin >> input_command >> val;
+
+        if (input_command == commands[0]) {
+            myThree.Insert(myThree.root, val);
+
+        } else if (input_command == commands[1]) {
+            myThree.Delete(val);
+
+        } else if (input_command == commands[2]) {
+            if (myThree.Search(myThree.root, val) != nullptr) {
+                std::cout << "true\n";
+            } else {
+                std::cout << "false\n";
+            }
+
+        } else if (input_command == commands[3]) {
+            if (myThree.Next(myThree.root, val) != nullptr) {
+                std::cout << myThree.Next(myThree.root, val)->data << "\n";
+            } else {
+                std::cout << "none\n";
+            }
+
+        } else if (input_command == commands[4]) {
+            if (myThree.Prev(myThree.root, val) != nullptr) {
+                std::cout << myThree.Prev(myThree.root, val)->data << "\n";
+            } else {
+                std::cout << "none\n";
+            }
+        }
+    }
     return 0;
 }
